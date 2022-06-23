2ec3324d30023c315011cf824ca1c59cb1008e09
Naftali Kulik <nkulik1994@gmail.com>
2022-06-23 11:26:59 -0400

    commit tree 6cec923619b78700ee16cd055dcfaa66ebe04b41

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+require_relative './user'
 
+class Student < User
+    attr_accessor :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn cool_facts
+        knowledge << cool_facts
+    end
 end
